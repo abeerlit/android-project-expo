890632fa76f7d18930c2a23d911b3c8d7d756b1a
Abeer Ahmad <44231761+AbeerAhmad@users.noreply.github.com>
2026-07-17 00:20:32 +0500

    chore: app version update

--- app.config.ts
+++ app.config.ts
@@ -57,7 +57,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     owner: process.env.EXPO_OWNER ?? config.owner ?? "voxo",
     name: displayName,
     slug: easProjectSlug,
-    version: "2.0.54",
+    version: "2.0.56",
     orientation: "default",
     icon: iconPath,
     scheme: packageName,
@@ -70,7 +70,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
     android: {
       package: packageName,
-      versionCode: 142,
+      versionCode: 144,
       adaptiveIcon: {
         foregroundImage: iconPath,
         backgroundColor: splashBackground
